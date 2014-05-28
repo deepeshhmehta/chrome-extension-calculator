0f6de26248a24ad1502add5bb4e20e3e8bf12dc9
unknown <deepeshhmehta@gmail.com>
2014-05-28 16:02:03 +0530

    i guess this time

--- calc.js
+++ calc.js
@@ -4,16 +4,39 @@ $(document).ready(function(){
 	operation = 0;
 	$('.digit').on("click",function(){
 		digiclick($(this).find('input').val());
-		showstats();
+		
 	});
 
 	$('.oper').on("click", function(){
-		if ($(this).find('input').val() == '=')
+		if ($(this).find('input').val() === '='){
 			evaluate();
+		
+		}
 		else
 		operclick($(this).find('input').val());
-		showstats();
+		
 	});
+$(document).keypress(function(e){
+	switch (e.which){
+		case 48:digiclick(0);break;
+		case 49:digiclick(1);break;
+		case 50:digiclick(2);break;
+		case 51:digiclick(3);break;
+		case 52:digiclick(4);break;
+		case 53:digiclick(5);break;
+		case 54:digiclick(6);break;
+		case 55:digiclick(7);break;
+		case 56:digiclick(8);break;
+		case 57:digiclick(9);break;
+		case 43:operclick('+');break;
+		case 45:operclick('-');break;
+		case 42:operclick('*');break;
+		case 47:operclick('/');break;
+		case 13:evaluate();break;
+		case 32:reset();break;
+	}
+});
+
 });
 
 
@@ -45,7 +68,7 @@ function operclick(x){
 	
 	$('#display').val(x);
 	switch (x){
-		case '+': operation = 1; console.log('recognized');break;
+		case '+': operation = 1;break;
 		case '-': operation = 2;break;
 		case '*': operation = 3;break;
 		case '/': operation = 4;break;
@@ -63,9 +86,10 @@ function evaluate(){
 	$('#display').val(answer);
 }
 
-function showstats(){
-	console.log(operation+" --oper");
-	console.log(temp+" -- temp");
-	console.log(answer+" -- answer");
-	console.log("---");
+function reset(){
+	temp = 0;
+	answer = 0;
+	operation = 0;
+	$('#display').val('0');
+
 }
