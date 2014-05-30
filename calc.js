00597496f064b323eea243c7d6a557e73dbef033
unknown <deepeshhmehta@gmail.com>
2014-05-30 12:32:05 +0530

    updated deletion of charecters using backspace, added input of point using keyboard

--- calc.js
+++ calc.js
@@ -17,26 +17,31 @@ $(document).ready(function(){
 		
 	});
 $(document).keypress(function(e){
-	switch (e.which){
-		case 48:digiclick(0);break;
-		case 49:digiclick(1);break;
-		case 50:digiclick(2);break;
-		case 51:digiclick(3);break;
-		case 52:digiclick(4);break;
-		case 53:digiclick(5);break;
-		case 54:digiclick(6);break;
-		case 55:digiclick(7);break;
-		case 56:digiclick(8);break;
-		case 57:digiclick(9);break;
-		case 43:operclick('+');break;
-		case 45:operclick('-');break;
-		case 42:operclick('*');break;
-		case 47:operclick('/');break;
-		case 13:evaluate();break;
-		case 32:reset();break;
-	}
+	console.log(e.which);
+		switch (e.which){
+			case 48:digiclick(0);break;
+			case 49:digiclick(1);break;
+			case 50:digiclick(2);break;
+			case 51:digiclick(3);break;
+			case 52:digiclick(4);break;
+			case 53:digiclick(5);break;
+			case 54:digiclick(6);break;
+			case 55:digiclick(7);break;
+			case 56:digiclick(8);break;
+			case 57:digiclick(9);break;
+			case 43:operclick('+');break;
+			case 45:operclick('-');break;
+			case 42:operclick('*');break;
+			case 47:operclick('/');break;
+			case 46:digiclick('.');break;
+			case 13:evaluate();break;
+			case 32:reset();break;
+		}
+	});
+$(document).keyup(function(e){
+	if (e.which===8)
+		backspace();
 });
-
 });
 
 
@@ -48,11 +53,11 @@ function digiclick(x){
 			case '*':
 			case '/': $('#display').val("");
 		}
-		if (parseInt($('#display').val())==0)
+		if (parseFloat($('#display').val())===0)
 			$('#display').val("");
+
 		$('#display').val($('#display').val()+x);
-		temp = parseInt($('#display').val());
-		// $('#display').val($(this).val());
+		temp = parseFloat($('#display').val());
 	}
 
 function operclick(x){
@@ -77,13 +82,15 @@ function operclick(x){
 
 function evaluate(){
 	switch (operation){
-			case 1: answer = parseInt(answer) + parseInt(temp);break;
-			case 2: answer =parseInt(answer)-parseInt(temp);break;
-			case 3: answer = parseInt(answer)*parseInt(temp);break;
-			case 4: answer = parseInt(answer)/parseInt(temp);break;
+			case 1: answer = parseFloat(answer) + parseFloat(temp);break;
+			case 2: answer =parseFloat(answer)-parseFloat(temp);break;
+			case 3: answer = parseFloat(answer)*parseFloat(temp);break;
+			case 4: answer = parseFloat(answer)/parseFloat(temp);break;
+			default: answer = temp;
 	}
 	temp=0;
 	$('#display').val(answer);
+
 }
 
 function reset(){
@@ -92,4 +99,13 @@ function reset(){
 	operation = 0;
 	$('#display').val('0');
 
+}
+
+function backspace(){
+	var str = $('#display').val();
+	console.log(str + "--" +str.length);
+	str = str.slice(0,str.length-1);
+	console.log(str);
+	$('#display').val(str);
+	temp = $('#display').val();
 }
